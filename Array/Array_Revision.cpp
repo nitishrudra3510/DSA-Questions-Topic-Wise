70dc6626029ab1049306f38bf9230ec8c24d34fd
nitishrudra3510 <114504003+nitishrudra3510@users.noreply.github.com>
2024-09-05 00:33:15 +0530

    Revision

--- Array/Array_Revision.cpp
+++ Array/Array_Revision.cpp
@@ -364,31 +364,59 @@
 
 
 // another methods
+// #include<iostream>
+// using namespace std;
+// void leftRotateD(int arr[], int size, int d){
+//     int temp[d];
+//     for(int i=0; i<d; i++){
+//         temp[i]=arr[i];
+//     }
+//     for(int i =d; i<size; i++){
+//         arr[i-d]=arr[i];
+//     }
+//     for(int i=0; i<d; i++){ 
+//         arr[size-d+i]=temp[i];
+//     }
+// }
+// int main(){
+//     int arr[] = {1,2,3,4,5,6,7,8,9,10,11};
+//     int size = sizeof(arr) / sizeof(arr[0]);
+//     int d;
+//     cin >> d;
+//     leftRotateD(arr, size, d);
+//     for(int i=0; i<size; i++){
+//         cout<<arr[i]<<" ";
+//     }
+// }
+
+
+// // T.c : theata(n)
+// // auxoloary space: Theta(d);
+
 #include<iostream>
+#include<algorithm>
 using namespace std;
+void reverse(int arr[], int low, int high);
 void leftRotateD(int arr[], int size, int d){
-    int temp[d];
-    for(int i=0; i<d; i++){
-        temp[i]=arr[i];
-    }
-    for(int i =d; i<size; i++){
-        arr[i-d]=arr[i];
-    }
-    for(int i=0; i<d; i++){ 
-        arr[size-d+i]=temp[i];
+    reverse(arr, 0, d-1);
+    reverse(arr, d, size-1);
+    reverse(arr, 0, size-1);
+}
+void reverse(int arr[],  int low, int high){
+    while(low<=high){
+        swap(arr[low], arr[high]);
+        low++;
+        high--;
     }
 }
 int main(){
-    int arr[] = {1,2,3,4,5,6,7,8,9,10,11};
+    int arr[] = {1,2,3,4,5,6};
     int size = sizeof(arr) / sizeof(arr[0]);
+    
     int d;
-    cin >> d;
+    cin>>d;
     leftRotateD(arr, size, d);
     for(int i=0; i<size; i++){
         cout<<arr[i]<<" ";
     }
 }
-
-
-// T.c : theata(n)
-// auxoloary space: Theta(d);
